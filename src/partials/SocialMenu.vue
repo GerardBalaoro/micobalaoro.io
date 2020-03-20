d08a3c59edac12c2aa8bb60db05403b1ad09af4d
GerardBalaoro <gmbalaoro@outlook.com>
2020-03-21 01:24:27 +0800

    Fix social icons

--- src/partials/SocialMenu.vue
+++ src/partials/SocialMenu.vue
@@ -2,7 +2,7 @@
     <ul :class="'text-gray-400 flex items-center' + className">
         <li class="px-2 py-1" v-for="(social, i) in $static.metadata.social" :key="i">
             <a :href="social.link" :title="social.name" target="_blank" class="hover:text-gray-100">
-                <fa :icon="social.icon" />
+                <fa :icon="social.icon.split('-')" />
             </a>
         </li>
     </ul>
